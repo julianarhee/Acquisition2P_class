be0e5c93475658378a814d416efcae1cacae5c8a
julianarhee <juliana.rhee@gmail.com>
2017-06-09 13:10:02 -0400

    fixed bad slice indexing

--- interleaveTiffs.m
+++ interleaveTiffs.m
@@ -1,22 +1,24 @@
-function interleaveTiffs(obj, info)
+function interleaveTiffs(obj)
 
-    nslices = length(info.(obj.acqName).correctedMovies.slice);
-    nchannels = length(info.(obj.acqName).correctedMovies.slice(1).channel);
-    nfiles = length(info.(obj.acqName).correctedMovies.slice(1).channel(1).fileName);
+    nslices = length(obj.correctedMovies.slice);
+    nchannels = length(obj.correctedMovies.slice(1).channel);
+    nfiles = length(obj.correctedMovies.slice(1).channel(1).fileName);
 
-    movsize = info.(obj.acqName).correctedMovies.slice(1).channel(1).size(1,:);
+    movsize = obj.correctedMovies.slice(1).channel(1).size(1,:);
     nframes = nslices*movsize(3)*nchannels;
 
-
+    sliceidxs = 1:2:nslices*nchannels;
     for file=1:nfiles
         newtiff = zeros(movsize(1), movsize(2), nframes);
         for slice = 1:nslices
-            currtiff = info.(obj.acqName).correctedMovies.slice(slice).channel(1).fileName{file};
-            [newtiff(:,:,slice:(nslices*2):end), ~] = tiffRead(currtiff);
-            currtiff = info.(obj.acqName).correctedMovies.slice(slice).channel(2).fileName{file};
-            [newtiff(:,:,(slice+1):(nslices*2):end), ~] = tiffRead(currtiff);
+            currtiff = obj.correctedMovies.slice(slice).channel(1).fileName{file};
+            [tmp,~] = tiffRead(currtiff);
+            newtiff(:,:,sliceidxs(slice):(nslices*2):end) = tmp;
+            currtiff = obj.correctedMovies.slice(slice).channel(2).fileName{file};
+            [tmp,~] = tiffRead(currtiff); 
+            newtiff(:,:,(sliceidxs(slice)+1):(nslices*2):end) = tmp;
         end
-        [fpath, fname, fext] = fileparts(info.(obj.acqName).correctedMovies.slice(slice).channel(1).fileName{file});
+        [fpath, fname, fext] = fileparts(obj.correctedMovies.slice(slice).channel(1).fileName{file});
         filename = strsplit(fname, '_');
         newtiffname = strcat(filename{end}, fext)
         tiffWrite(newtiff, newtiffname, obj.defaultDir, 'int16');
