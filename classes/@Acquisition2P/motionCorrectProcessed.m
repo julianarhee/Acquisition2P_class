f0c6780112af5b28f26ef3fe4a16730b5296e6ec
Juliana <julianarhee@gmail.com>
2017-10-26 14:02:20 -0400

    clean up motionCorrectProcessed function. use read_file instead of default tiffRead to ignore metadata warning

--- classes/@Acquisition2P/motionCorrectProcessed.m
+++ classes/@Acquisition2P/motionCorrectProcessed.m
@@ -96,8 +96,8 @@ for movNum = movieOrder
     % but movie file comes from processed tiff:
     fprintf('Curr movie is: %s\n', obj.Movies{movNum});
     if strfind(scanImageMetadata.SI.VERSION_MAJOR, '2016')
-        [mov, ~] = tiffRead(obj.Movies{movNum});
-        %mov = read_file(obj.Movies{movNum});
+        %[mov, ~] = tiffRead(obj.Movies{movNum});
+        mov = read_file(obj.Movies{movNum});
     else
         mov = read_imgdata(obj.Movies{movNum});
     end
@@ -107,15 +107,14 @@ for movNum = movieOrder
 
     fprintf('Pixels Per Line: %i\n', scanImageMetadata.SI.hRoiManager.pixelsPerLine)
     fprintf('Lines Per Frame: %i\n', scanImageMetadata.SI.hRoiManager.linesPerFrame)
-    if size(mov,2)~=scanImageMetadata.SI.hRoiManager.pixelsPerLine
-        fprintf('Cropping pixels per line to %i\n', size(mov,2));
-        scanImageMetadata.SI.hRoiManager.pixelsPerLine = size(mov,2);
-    end
-    if size(mov,1)~=scanImageMetadata.SI.hRoiManager.linesPerFrame
-        fprintf('Cropping lines per frame to %i\n', size(mov,1));
-        scanImageMetadata.SI.hRoiManager.linesPerFrame = size(mov,1);
-    end
-
+%     if size(mov,2)~=scanImageMetadata.SI.hRoiManager.pixelsPerLine
+%         fprintf('Cropping pixels per line to %i\n', size(mov,2));
+%         scanImageMetadata.SI.hRoiManager.pixelsPerLine = size(mov,2);
+%     end
+%     if size(mov,1)~=scanImageMetadata.SI.hRoiManager.linesPerFrame
+%         fprintf('Cropping lines per frame to %i\n', size(mov,1));
+%         scanImageMetadata.SI.hRoiManager.linesPerFrame = size(mov,1);
+%     end
 
     if obj.binFactor > 1
         mov = binSpatial(mov, obj.binFactor);
@@ -123,77 +122,12 @@ for movNum = movieOrder
 
     % Adjust SI meta to match true data:
     scanImageMetadata = adjust_si_metadata(scanImageMetadata, size(mov));
-%    fprintf('Parsing processed SI tiff and getting adjusted meta data...\n');
-%    fprintf('Size of movie: %s\n', mat2str(size(mov)));
-%    nSlicesTmp = scanImageMetadata.SI.hStackManager.numSlices
-%    nDiscardTmp = scanImageMetadata.SI.hFastZ.numDiscardFlybackFrames;
-%    nVolumesTmp = scanImageMetadata.SI.hFastZ.numVolumes;
-%    nChannelsTmp = numel(scanImageMetadata.SI.hChannels.channelSave);
-%    desiredSlices = (size(mov, 3) / nChannelsTmp) / nVolumesTmp
-%    nDiscardedExtra = nSlicesTmp - desiredSlices
-%    if desiredSlices ~= nSlicesTmp  % input (processed) tiff does not have discard removed, or has extra flyback frames removed.
-% 	if nDiscardTmp == 0
-% 	    % This means discard frames were not specified and acquired, and flyback frames removed from top in processed tiff.
-% 	    extra_flyback_top = true;
-%             nDiscardTmp = nSlicesTmp - desiredSlices;
-% 	    false_discard = true;
-%         elseif nDiscardTmp > 0
-% 	    % Discard frames were specified/acquired but extra flyback frames removed from top of stack
-% 	    extra_flyback_top = true;   
-% 	    false_discard = false;
-% 	end
-%     else
-% 	extra_flyback_top = false;
-% 	false_discard = false;
-%     end
-% 
-%    nSlicesSelected = desiredSlices; %nSlicesTmp - nDiscardTmp;
-% 
-%    scanImageMetadata.SI.hStackManager.numSlices = nSlicesSelected;
-%    scanImageMetadata.SI.hFastZ.numDiscardFlybackFrames = 0;
-%    scanImageMetadata.SI.hFastZ.numFramesPerVolume = scanImageMetadata.SI.hStackManager.numSlices;
-%    scanImageMetadata.SI.hStackManager.zs = scanImageMetadata.SI.hStackManager.zs(nDiscardedExtra+1:end);
-%    scanImageMetadata.SI.hFastZ.discardFlybackFrames = 0;  % Need to disflag this so that parseScanimageTiff (from Acquisition2P) takes correct n slices
-%    nFramesSelected = nChannelsTmp*nSlicesSelected*nVolumesTmp
-% 
-%    metanames = fieldnames(scanImageMetadata);
-%    for field=1:length(metanames)
-%        if strcmp(metanames{field}, 'SI')
-%            continue;
-%        else
-%            currfield = scanImageMetadata.(metanames{field});
-%             if extra_flyback_top && false_discard %falseDiscard
-%                 % there are no additional empty flybacks at the end of volume, so just skip every nSlicesTmp, starting from corrected num nDiscard removed from top:
-%                 startidxs = colon(nDiscardTmp*nChannelsTmp+1, nChannelsTmp*(nSlicesTmp), length(currfield)); 
-%                 fprintf('N volumes based on start indices: %i\n', length(startidxs));
-%             elseif extra_flyback_top && ~false_discard
-% 		% There were specified num of empty flybacks at end of volume, so remove those indices, if necessary, while also removing the removed frames at top:
-%                 startidxs = colon(nDiscardTmp*nChannelsTmp+1, nChannelsTmp*(nSlicesTmp+nDiscardTmp), length(currfield));
-%             else
-% 		% There were empty flyybacks at end of volume, but correctly executed, s.t. no additional flybacks removed from top:
-% 		startidxs = colon(1:nChannelsTmp*(nSlicesTmp+nDiscardTmp), length(currfield)); 
-% 	    end
-%            if iscell(currfield)
-%                tmpfield = cell(1, nFramesSelected);
-%            else
-%                tmpfield = zeros(1, nFramesSelected);
-%            end
-%            newidx = 1;
-%            for startidx = startidxs
-% 	       % Only grab info for those frames that are kept in processed tiff (i.e., remove indices related to any "top" flyback and any "true" discard frames:
-%                tmpfield(newidx:newidx+(nSlicesSelected*nChannelsTmp - 1)) = currfield(startidx:startidx+(nSlicesSelected*nChannelsTmp - 1));
-%                newidx = newidx + (nSlicesSelected*nChannelsTmp);
-%            end
-%            scanImageMetadata.(metanames{field}) = tmpfield;
-%        end
-%    end
-% 
 
 
     % Apply line shift:
     fprintf('Line Shift Correcting Movie #%03.0f of #%03.0f\n', movNum, nMovies),
     mov = correctLineShift(mov);
-    fprintf('Finished Line-Shift correction. Movie size is: %s', mat2str(size(mov)));
+    fprintf('Finished Line-Shift correction. Movie size is: %s\n', mat2str(size(mov)));
     try
         [movStruct, nSlices, nChannels] = parseScanimageTiff(mov, scanImageMetadata);
 	fprintf('Parsed SI tiff. nSlices: %s', num2str(nSlices));
@@ -222,50 +156,28 @@ for movNum = movieOrder
             fprintf('Writing Movie #%03.0f of #%03.0f\n',movNum,nMovies),
             try
                 tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, movFileName, writeDir, 'int16');
-                %tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, movFileName, writeDir, 'uint16');
             catch
                 % Sometimes, disk access fails due to intermittent
                 % network problem. In that case, wait and re-try once:
                 pause(60);
                 tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, movFileName, writeDir, 'int16');
-                %tiffWrite(movStruct.slice(nSlice).channel(nChannel).mov, movFileName, writeDir, 'uint16');
             end
         end
     end
 
-    obj.metaDataSI{movNum} = scanImageMetadata;
+    obj.metaDataSI{movNum} = scanImageMetadata; % Store adjusted SI metadata for EACH file
 end
 
-% Store SI metadata in acq object
-%obj.metaDataSI = scanImageMetadata;
-
-%Assign acquisition to a variable with its own name, and write to same
-%directory
+%Assign acquisition to a variable with its own name, and write to same directory
 eval([obj.acqName ' = obj;']),
-%save(fullfile(obj.defaultDir, obj.acqName), obj.acqName)
+
 % Rename Acq2P object because want to create SI meta struct for processed TIFFs:
-newObjName = sprintf('Acq2P2P_%s.mat', obj.acqName);
+newObjName = sprintf('Acq2P_%s.mat', obj.acqName);
 save(fullfile(obj.defaultDir, newObjName), obj.acqName)
 
 %movefile(fullfile(obj.defaultDir, [obj.acqName '.mat']), fullfile(obj.defaultDir, newObjName));
 fprintf('Renamed Acq2P obj file to: %s\n', newObjName);
 display('Motion Correction Completed!')
-
-
-% if ~exist(fullfile(obj.defaultDir, 'Raw'), 'dir')
-%     mkdir(fullfile(obj.defaultDir, 'Raw'));
-% end
-% 
-% for movidx=1:length(obj.Movies)
-%     [datadir, fname, ext] = fileparts(obj.Movies{movidx});
-%     movefile(obj.Movies{movidx}, fullfile(obj.defaultDir, 'Raw', strcat(fname, ext)));
-% end
-% 
-%movefile(fullfile(obj.defaultDir, strcat(obj.acqName, '.mat')), fullfile(obj.defaultDir, 'Corrected', strcat(obj.acqName, '.mat')));
-
-%info = load(fullfile(obj.defaultDir, 'Corrected', strcat(obj.acqName, '.mat')))
-
-%interleaveTiffs(obj, false); %, info);
 
 end
 
