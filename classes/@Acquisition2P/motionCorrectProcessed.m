5ecff4a44731757be7585e9c2e404aecb843110f
julianarhee <juliana.rhee@gmail.com>
2017-12-07 11:27:44 -0500

    fixed strcat appending process dir hash to processed dir in whcih to find raw flyback dir

--- classes/@Acquisition2P/motionCorrectProcessed.m
+++ classes/@Acquisition2P/motionCorrectProcessed.m
@@ -81,16 +81,17 @@ if ~any(strfind(tiffsrc, 'raw')) % Tiffs are not themselves a 'raw' source
         pid_info = loadjson(fullfile(processed_dir, pinfo_fn(1).name)); 
         if ismember(process_name, fieldnames(pid_info))
             proc_tiff_source = pid_info.(process_name).PARAMS.preprocessing.sourcedir;
-            proc_hash = pid_info.(process_name).tmp_hashid;
+            proc_hash = pid_info.(process_name).tmp_hashid
         else
             proc_tiff_source = pid_info.(process_name_parts{1}).PARAMS.preprocessing.sourcedir;
-            proc_hash = pid_info.(process_name_parts{1}).tmp_hashid;
+            proc_hash = pid_info.(process_name_parts{1}).tmp_hashid
         end
         fprintf('Processed tiffsrc came from: %s\n', proc_tiff_source); 
-        [proc_dir, proc_folder, ~] = fileparts(proc_tiff_source);
-        if ~any(strfind(proc_folder, proc_hash))
-            proc_dir = proc_dir + sprintf('_%s', proc_hash);
+        [proc_dir, proc_rawfolder, ~] = fileparts(proc_tiff_source);
+        if ~any(strfind(proc_dir, proc_hash))
+            proc_dir = [proc_dir sprintf('_%s', proc_hash)];
         end
+        fprintf('Looking in processed dir %s for raw\n', proc_dir);
         simeta_source = dir(fullfile(proc_dir, 'raw*')); % cuz might have hash added
         simeta_source = fullfile(proc_dir, simeta_source(1).name); %orig_tiff_source;
         fprintf('Loading SIMETA info from PROCESSED src: %s\n', simeta_source);
@@ -99,20 +100,6 @@ if ~any(strfind(tiffsrc, 'raw')) % Tiffs are not themselves a 'raw' source
         fprintf('ABORTING MOTION CORRECTION STEP.');
         abort = true;
     end
-%    % Parent dir of tiffs is not a 'raw' source:
-%    simeta_source = dir(fullfile(obj.defaultDir, 'raw*'));
-%    if length(simeta_source)==0
-%        % Parent dir of tiffs is inside of current PID dir:
-%        % BUT, no 'raw_<id>' dir found, so use RAW src from current run:
-%        [processdir, processfolder, ~] = fileparts(obj.defaultDir);
-%        [rundir, processfolder, ~] = fileparts(processdir);
-%        simeta_source = dir(fullfile(rundir, 'raw*'));
-%        simeta_source = fullfile(rundir, simeta_source(1).name);
-%        fprintf('Extracting from RAW simeta: %s\n', simeta_source);
-%    else
-%        fprintf('Extracting from PROCESSED raw simeta: %s\n', simeta_source(1).name);
-%        simeta_source = fullfile(obj.defaultDir, simeta_source(1).name);
-%    end
 else
     % TIffs are from a 'raw' source:
     fprintf('Loading RAW SIMETA from raw src: %s\n', tiffsrc);
